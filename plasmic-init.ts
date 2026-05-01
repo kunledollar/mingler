be8b558772edb04b5801f9f2bec96dc1989d6ea8
Kunle <ebony4pink@gmail.com>
2026-05-01 03:06:54 -0400

    Initialize Mingler + Plasmic project

---
+++ plasmic-init.ts
@@ -0,0 +1,30 @@
+import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
+
+export const PLASMIC = initPlasmicLoader({
+  projects: [
+    {
+      id: "tLJbxbBKHWewdoA87spCXQ",
+      token: "HZMZidS089iy11yZQC4aZ8jDTOuztQW1KCXPGaSKMZGofyM1rs0itbLFrFmsHe5QijPO9CaT7YwDV2yoEQ",
+    },
+  ],
+  platformOptions: {
+    nextjs: {
+      appDir: false,
+    }
+  },
+  
+  // By default Plasmic will use the last published version of your project.
+  // For development, you can set preview to true, which will use the unpublished
+  // project, allowing you to see your designs without publishing.  Please
+  // only use this for development, as this is significantly slower.
+  preview: false,
+});
+
+// You can register any code components that you want to use here; see
+// https://docs.plasmic.app/learn/code-components-ref/
+// And configure your Plasmic project to use the host url pointing at
+// the /plasmic-host page of your nextjs app (for example,
+// http://localhost:3000/plasmic-host).  See
+// https://docs.plasmic.app/learn/app-hosting/#set-a-plasmic-project-to-use-your-app-host
+
+// PLASMIC.registerComponent(...);
